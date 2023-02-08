ae46c56c71fd453053c8baa016cc2ad3f54b7c86
Your Name <iyanperezmarcos@gmail.com>
2023-02-08 15:22:35 +0100

    router guard started

--- src/main/Frontend/src/router/index.js
+++ src/main/Frontend/src/router/index.js
@@ -1,20 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import  VueRouter  from "vue-router";
+import Vue from "vue";
 
-const router = createRouter({
+Vue.use(VueRouter)
+
+const routes = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {requiresAuth: true}
     },
     {
       path: "/dashboard",
       name: "dashboard",
       component: () => import("../views/DashboardView.vue"),
+      meta: {requiresAuth: true}
+
     },
   ],
 });
+
+
+const router = new VueRouter({
+  routes
+})
 
 export default router;
